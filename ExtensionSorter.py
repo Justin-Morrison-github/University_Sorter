3f215a2eae038f284bd56f1c6a06253a3fbeb8ca
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-09 23:48:09 -0500

    Removed extensions dict and put it into json file. Added Symbol support and folder key to file dict to hold path.

--- ExtensionSorter.py
+++ ExtensionSorter.py
@@ -1,73 +1,41 @@
 import json
 
-from move import send_file, user_continues, pretty_print_substring, return_pretty_print_string
+from move import user_continues, pretty_print_substring, return_pretty_print_string
 from colorama import Fore
 from pathlib import Path
 from Settings import Settings
+from Symbols import Symbol
 
 
 def main():
+    file = Path(__file__).stem
+
     settings_file = "JSON/settings.json"
     if not Path(settings_file).exists():
         raise FileNotFoundError(settings_file)
     with open(settings_file, 'r') as settings_json:
         settings: dict = json.load(settings_json)
 
-    extensions = {
-        # AUDIO
-        '.mp3': 'AUDIO\\MP3',
-        '.wav': 'AUDIO\\WAV',
-
-        # CODE
-        '.py':   'CODE\\PYTHON',
-        '.cs':   'CODE\\C#',
-        '.c':    'CODE\\C',
-        '.java': 'CODE\\JAVA',
-
-        # FOLDERS
-        '.zip': 'FOLDERS\\ZIP',
-        '':     'FOLDERS\\Folders',
-
-        # IMAGES
-        '.pdf':  'IMAGES\\PDF',
-        '.png':  'IMAGES\\PNG',
-        '.jpg':  'IMAGES\\JPG',
-        '.ico':  'IMAGES\\ICON',
-        '.HEIC': 'IMAGES\\HEIC',
-
-        # INSTALLERS
-        '.exe': 'INSTALLERS\\EXE',
-        '.msi': 'INSTALLERS\\MSI',
-
-        # OTHER
-        '.f3d':           'OTHER\\FUSION',
-        '.otf':           'OTHER\\FONTS',
-        '.metadata':      'OTHER\\METADATA',
-        '.tracktionedit': 'OTHER\\TRACKTION',
-        '.vsix':          'OTHER\\VSIX',
-
-        # TEXT
-        '.txt':  'TEXT\\TXT',
-        '.docx': 'TEXT\\WORD',
-        '.xlsx': 'TEXT\\EXCEL',
-        '.ppt':  'TEXT\\POWERPOINT',
-        '.pptx': 'TEXT\\POWERPOINT'
-    }
+    with open(settings[file][Settings.JSON_FILE], 'r') as extension_file:
+        extensions = json.load(extension_file)
 
     if Path.cwd().anchor == '/':
         srcFolder = Path(settings[file][Settings.WSL_SRC_PATH])
     else:
         srcFolder = Path(settings[file][Settings.WIN_SRC_PATH])
+
     files_to_be_sent = []
 
     for file in srcFolder.iterdir():
-        if file.suffix in extensions:
-            files_to_be_sent.append(
-                {
-                    "src":  srcFolder / file,
-                    "dst": srcFolder / Path(f"{(extensions[file.suffix])}/{file.name}"),
-                }
-            )
+        for folder in extensions:
+            if file.suffix in extensions[folder] and file.name not in extensions:
+                files_to_be_sent.append(
+                    {
+                        "src":  srcFolder / file,
+                        "dst": srcFolder / Path(f"{(extensions[folder][file.suffix])}/{file.name}"),
+                        "folder": folder
+                    }
+                )
 
     if len(files_to_be_sent) == 0:
         print("No files were found")
@@ -82,14 +50,41 @@ def main():
 
 def print_file(file: dict[str, Path], extensions: dict[str, str]):
     src_string = return_pretty_print_string(str(file["src"]), str(file["src"].name), start="", color=Fore.CYAN)
-    pretty_print_substring(src_string, file["src"].suffix, start="\u2794  From:  ", color=Fore.YELLOW)
+    pretty_print_substring(src_string, file["src"].suffix, start=f"{Symbol.ARROW}  From:  ", color=Fore.YELLOW)
 
     dst_string = return_pretty_print_string(str(file["dst"]), str(file["dst"].name), start="", color=Fore.CYAN)
 
-    ext_path = extensions[file["src"].suffix].split("\\")[-1]
+    ext_path = extensions[file['folder']][file["src"].suffix].split("\\")[-1]
     dst_string = return_pretty_print_string(dst_string, file["src"].suffix, start="", color=Fore.YELLOW)
 
-    pretty_print_substring(dst_string, ext_path, start="\u2794    To:  ", end='\n', color=Fore.YELLOW)
+    pretty_print_substring(dst_string, ext_path, start=f"{Symbol.ARROW}    To:  ", end='\n', color=Fore.YELLOW)
+
+
+def send_file(src: Path, dst: Path, send_enabled=False) -> None:
+    """
+    Sends a file from src to dst. Prints out certain results
+    """
+
+    try:
+        if dst.exists():
+            print(Fore.GREEN + f'{Symbol.SUCCESS} From:  {src}')
+            print(Fore.RED + f'{Symbol.FAILURE}   To:  {dst}')
+            print(Fore.YELLOW + f"WARNING: File Already Exists")
+        else:
+            if send_enabled:
+                src.rename(dst)
+            print(Fore.GREEN + f'{Symbol.SUCCESS} From:  {src}')
+            print(Fore.GREEN + f'{Symbol.SUCCESS}   To:  {dst}', end="")
+
+    except FileNotFoundError as e:
+        print(Fore.YELLOW + f'{Symbol.FAILURE} From:  {src}')
+        print(Fore.YELLOW + f'{Symbol.FAILURE}   To:  {dst}')
+        print(Fore.RED + f"ERROR: File Not Found {e}")
+
+    except Exception as error:
+        print(error)
+
+    print(Fore.RESET + "\n")
 
 
 if __name__ == "__main__":

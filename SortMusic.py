8db354a48ca1d018aa0a3dd1f80b240076b6f23e
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-10 00:00:39 -0500

    Added json settings and WSL path support

--- SortMusic.py
+++ SortMusic.py
@@ -1,20 +1,31 @@
-# import os
+import json
 from pathlib import Path
 from move import send_file, user_continues
-
+from Settings import Settings
 
 def main():
+    file = Path(__file__).stem
 
-    destination = Path("C:\\Users\\morri\\OneDrive\\Music\\Songs")
-    src_folder = Path("C:\\Users\\morri\\Downloads\\AUDIO\\MP3")
+    settings_file = "JSON/settings.json"
+   
+    with open(settings_file, 'r') as settings_json:
+        settings: dict = json.load(settings_json)
+
+    if Path.cwd().anchor == '/':
+        dst_folder = Path(settings[file][Settings.WSL_DST_PATH])
+        src_folder = Path(settings[file][Settings.WSL_SRC_PATH])
+    else:
+        dst_folder = Path(settings[file][Settings.WIN_DST_PATH])
+        src_folder = Path(settings[file][Settings.WIN_SRC_PATH])
+
     files_to_be_sent = []
 
     for file in src_folder.iterdir():
-        if file.suffix == ".mp3":
+        if file.suffix in settings[file][Settings.PATHS_TO_CHECK]:
             files_to_be_sent.append(
                 {
                     "src": src_folder / file.name,
-                    "dst": destination / file.name
+                    "dst": dst_folder / file.name
                 }
             )
 

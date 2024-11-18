09ff0e9d5981922cecf7a4e4eb7ad1915356f93f
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-18 17:02:24 -0500

    Removed os module dependence, changed to pathlib. Added unique print_hierarchy function from move into this file.

--- Heirarchy.py
+++ Heirarchy.py
@@ -1,24 +1,49 @@
-import os
+from pathlib import Path
 import tkinter
 from tkinter import filedialog
-from move import print_hierarchy, get_folder_size
+from move import get_folder_size
 from sys import argv
 from colorama import Fore
-
+from Symbols import Symbol
 
 def main():
     init_dir = "C:\\Users\\morri\\Downloads"
 
     if len(argv) == 1:  # No folder given
         tkinter.Tk().withdraw()  # prevents an empty tkinter window from appearing
-        folder = filedialog.askdirectory(initialdir=init_dir)
-        print(Fore.YELLOW + f"\n{os.path.basename(folder)}" + Fore.RESET, get_folder_size(folder))
+        folder = Path(filedialog.askdirectory(initialdir=init_dir))
+        print(Fore.YELLOW + f"\n{folder.stem}" + Fore.RESET, get_folder_size(folder))
 
         print_hierarchy(folder, arrow_color=Fore.WHITE, count_color=Fore.WHITE)
 
     else:  # Folder given
         print("Error")
         return 1
+
+def print_hierarchy(
+        src_folder_path: str, depth=0, arrow_color=Fore.BLACK, count_color=Fore.BLACK, file_color=Fore.CYAN,
+        size_color=Fore.GREEN, show_arrow=True, show_count=True, indent="\t", deci_places=2):
+    depth += 1
+    count = 0
+    for file in Path(src_folder_path).iterdir():
+        path = Path(src_folder_path) / Path(file).name
+        if path.is_dir():
+            print(f"{indent * (depth-1)}", end=" ")
+
+            if (show_arrow):
+                print(f"{arrow_color}{Symbol.ARROW}{Fore.RESET}", end=" ")
+
+            count += 1
+            if (show_count):
+                print(f"{count_color}{count:03d}{Fore.RESET}", end=" ")
+
+            print(f'{file_color}{file.name}{Fore.RESET}', end="")
+
+            size = get_folder_size(path)
+            print(f"{size_color}{size: .{deci_places}f}{Fore.RESET}"),
+
+            print_hierarchy(path, depth, arrow_color, count_color, file_color,
+                            size_color, show_arrow, show_count, indent, deci_places)
 
 
 if __name__ == '__main__':

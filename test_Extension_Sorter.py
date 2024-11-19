d4cb8c03a846e031e8f682cb3613a898526bf0aa
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-19 00:15:11 -0500

    Wrote some basic tests to figure how to mock/simulate file I/O operations. Will improve in future

---
+++ test_Extension_Sorter.py
@@ -0,0 +1,64 @@
+import unittest
+from pathlib import Path
+import tempfile
+from ExtensionSorter import get_files_to_be_sent, set_up_settings
+
+
+class TestExtensionSorter(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.extensions = {
+            "AUDIO": {
+                ".mp3": "AUDIO/MP3",  # Use forward slashes for cross-platform paths
+                ".wav": "AUDIO/WAV"
+            },
+        }
+    
+    def test_get_files_to_be_sent_with_tempfile(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            root = Path(temp_dir)
+            src_file_1 = root / "test.mp3"
+            src_file_2 = root / "test.wav"
+            
+            audio = root / "AUDIO"
+            mp3 = audio / "MP3"
+            wav = audio / "WAV"
+            dst_file_1 = mp3 / "test.mp3"
+            dst_file_2 = wav / "test.wav"
+
+            # Create source file
+            src_file_1.write_text("Mock file 1.")
+            src_file_2.write_text("Mock file 2.")
+
+            expected_file_to_be_sent = [{
+                "src": src_file_1,
+                "dst": dst_file_1
+            }, {
+                "src": src_file_2,
+                "dst": dst_file_2
+            }]
+
+            files_to_be_sent = get_files_to_be_sent(self.extensions, root)
+
+            self.assertEqual(files_to_be_sent, expected_file_to_be_sent)
+
+            # # Check if the file was moved (optional: if the function moves files)
+            # self.assertTrue(dst_file_path.exists())
+            # self.assertFalse(src_file_path.exists())  # Ensure the source file no longer exists
+
+    def test_set_up_settings(self):
+        expected_settings =  {
+            "json_file": "JSON/extensions.json",
+            "win_src_path": "C:/Users/morri/Downloads",
+            "wsl_src_path": "/mnt/c/Users/morri/Downloads"
+        }
+
+        actual_settings = set_up_settings("JSON/settings.json", "ExtensionSorter")
+
+        self.assertEqual(actual_settings, expected_settings)
+
+            
+       
+if __name__ == "__main__":
+    unittest.main()

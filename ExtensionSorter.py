285b11ca9b09eff1fdd61917145f72b14fffc509
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-19 00:14:19 -0500

    Seperated some functionality into functions to make it easier to test. Also got rid of need to check settings[file] fince settings get initilized for the current file already. Also removed the folder key of the files_to_be_sent since I found to a way to not use it.

--- ExtensionSorter.py
+++ ExtensionSorter.py
@@ -9,33 +9,18 @@ from Symbols import Symbol
 
 def main():
     file = Path(__file__).stem
+    
+    settings = set_up_settings("JSON/settings.json", file)
 
-    settings_file = "JSON/settings.json"
-    if not Path(settings_file).exists():
-        raise FileNotFoundError(settings_file)
-    with open(settings_file, 'r') as settings_json:
-        settings: dict = json.load(settings_json)
-
-    with open(settings[file][Settings.JSON_FILE], 'r') as extension_file:
+    with open(settings[Settings.JSON_FILE], 'r') as extension_file:
         extensions = json.load(extension_file)
 
     if Path.cwd().anchor == '/':
-        srcFolder = Path(settings[file][Settings.WSL_SRC_PATH])
+        srcFolder = Path(settings[Settings.WSL_SRC_PATH])
     else:
-        srcFolder = Path(settings[file][Settings.WIN_SRC_PATH])
+        srcFolder = Path(settings[Settings.WIN_SRC_PATH])
 
-    files_to_be_sent = []
-
-    for file in srcFolder.iterdir():
-        for folder in extensions:
-            if file.suffix in extensions[folder] and file.name not in extensions:
-                files_to_be_sent.append(
-                    {
-                        "src":  srcFolder / file,
-                        "dst": srcFolder / Path(f"{(extensions[folder][file.suffix])}/{file.name}"),
-                        "folder": folder
-                    }
-                )
+    files_to_be_sent = get_files_to_be_sent(extensions, srcFolder)
 
     if len(files_to_be_sent) == 0:
         print("No files were found")
@@ -48,13 +33,36 @@ def main():
                 send_file(file["src"], file["dst"], send_enabled=True)
 
 
+def set_up_settings(settings_file, current_file):
+    if not Path(settings_file).exists():
+        raise FileNotFoundError(settings_file)
+    with open(settings_file, 'r') as settings_json:
+        settings: dict = json.load(settings_json)
+    return settings[current_file]
+
+
+def get_files_to_be_sent(extensions, srcFolder):
+    files_to_be_sent = []
+    for file in srcFolder.iterdir():
+        for folder in extensions:
+            if file.suffix in extensions[folder] and file.name not in extensions:
+                files_to_be_sent.append(
+                    {
+                        "src":  srcFolder / file,
+                        "dst": srcFolder / Path(f"{(extensions[folder][file.suffix])}/{file.name}")
+                    }
+                )
+
+    return files_to_be_sent
+
+
 def print_file(file: dict[str, Path], extensions: dict[str, str]):
     src_string = return_pretty_print_string(str(file["src"]), str(file["src"].name), start="", color=Fore.CYAN)
     pretty_print_substring(src_string, file["src"].suffix, start=f"{Symbol.ARROW}  From:  ", color=Fore.YELLOW)
 
     dst_string = return_pretty_print_string(str(file["dst"]), str(file["dst"].name), start="", color=Fore.CYAN)
 
-    ext_path = extensions[file['folder']][file["src"].suffix].split("\\")[-1]
+    ext_path = extensions[file['dst'].parent.parent.name][file["src"].suffix].split("\\")[-1]
     dst_string = return_pretty_print_string(dst_string, file["src"].suffix, start="", color=Fore.YELLOW)
 
     pretty_print_substring(dst_string, ext_path, start=f"{Symbol.ARROW}    To:  ", end='\n', color=Fore.YELLOW)

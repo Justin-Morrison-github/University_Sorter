c0a98904b98df6ea871918539a32745fb7b5516f
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-20 21:23:36 -0500

    Renamed module to better describe what it does, and put actual unicode symbols to better understand what each character is

---
+++ ANSI.py
@@ -0,0 +1,12 @@
+from enum import StrEnum
+
+
+class ANSI(StrEnum):
+    ARROW = "⮡ "
+    STRAIGHT_ARROW = " ➔  "
+    SUCCESS = "☑ "
+    FAILURE = "☒ "
+    WARNING = "⚠ "
+    INDENT = "    "
+    B_FAILURE = "❌ "
+    B_SUCCESS = "✅ "

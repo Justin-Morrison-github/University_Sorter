94c400d98b50dcd736b77bf962b8883ab01255cc
Justin Morrison <morrisonxjustin@gmail.com>
2024-11-20 23:16:30 -0500

    Got rid of specific variables, not just have basepath, src_path, and dst_path and set those according to operating system.

--- Settings.py
+++ Settings.py
@@ -17,14 +17,17 @@ class Settings():
     def __init__(self, settings_file, current_file):
         with open(settings_file, 'r') as settings_json:
             settings: dict = json.load(settings_json)[current_file]
+        
+        if Path.cwd().anchor == '/':
+            self.basepath = Path(settings.get(Settings.WSL_BASEPATH, "NULL"))
+            self.src_path = Path(settings.get(Settings.WSL_SRC_PATH, "NULL"))
+            self.dst_path = Path(settings.get(Settings.WSL_DST_PATH, "NULL"))
+
+        else:
+            self.basepath = Path(settings.get(Settings.WIN_BASEPATH, "NULL"))
+            self.src_path = Path(settings.get(Settings.WIN_SRC_PATH, "NULL"))
+            self.dst_path = Path(settings.get(Settings.WIN_DST_PATH, "NULL"))
 
         self.json_file = Path(settings.get(Settings.JSON_FILE, "NULL"))
-        self.win_basepath = Path(settings.get(Settings.WIN_BASEPATH, "NULL"))
-        self.wsl_basepath = Path(settings.get(Settings.WSL_BASEPATH, "NULL"))
-        self.win_src_path = Path(settings.get(Settings.WIN_SRC_PATH, "NULL"))
-        self.wsl_src_path = Path(settings.get(Settings.WSL_SRC_PATH, "NULL"))
-        self.win_dst_path = Path(settings.get(Settings.WIN_DST_PATH, "NULL"))
-        self.wsl_dst_path = Path(settings.get(Settings.WSL_DST_PATH, "NULL"))
         self.paths_to_check = settings.get(Settings.PATHS_TO_CHECK, "NULL")
         self.root = "NULL"
-

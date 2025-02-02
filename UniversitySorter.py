8d7bb900619f545e26bf5deea677711aca8750df
Justin Morrison <morrisonxjustin@gmail.com>
2025-02-02 02:40:12 -0500

    Renamed some variables for better readability, extracted parts as seperate functions. Commented WSL settings setup up since it wasn't being uesd.

--- UniversitySorter.py
+++ UniversitySorter.py
@@ -15,14 +15,14 @@ def main():
     with open(settings_file, 'r') as settings_json:
         settings: dict = json.load(settings_json)
 
-    # If I am accessing these scripts from my WSL instance, will have different path structure
-    if Path.cwd().anchor == '/':
-        basepath = Path(settings[file][Settings.WSL_BASEPATH])
-        src_folder_path = Path(settings[file][Settings.WSL_SRC_PATH])
+    # # If I am accessing these scripts from my WSL instance, will have different path structure
+    # if Path.cwd().anchor == '/':
+    #     basepath = Path(settings[file][Settings.WSL_BASEPATH])
+    #     src_folder_path = Path(settings[file][Settings.WSL_SRC_PATH])
 
-    else:
-        basepath = Path(settings[file][Settings.WIN_BASEPATH])
-        src_folder_path = Path(settings[file][Settings.WIN_SRC_PATH])
+    # else:
+    basepath = Path(settings[file][Settings.WIN_BASEPATH])
+    src_folder_path = Path(settings[file][Settings.WIN_SRC_PATH])
 
     if not basepath.exists():
         raise FileNotFoundError(basepath)
@@ -32,12 +32,7 @@ def main():
     with open(settings[file][Settings.JSON_FILE], 'r') as json_file:
         university: dict = json.load(json_file)
 
-    classes = {}
-    for year, semesters in university.items():
-        for semester, class_list in semesters.items():
-            folder_path: Path = basepath / year / semester
-            class_paths: Path = get_class_paths(folder_path)
-            classes.update(dict(zip(class_list, class_paths)))
+    classes = create_class_dict(university, basepath)
 
     files_to_be_sent = traverse_folder(src_folder_path, classes)
 
@@ -52,31 +47,42 @@ def main():
             send_files(files_to_be_sent, send_enabled=True)
 
 
+def create_class_dict(university: dict, basepath: Path) -> dict:
+    classes = {}
+    for year, semesters in university.items():
+        for semester, class_list in semesters.items():
+            folder_path: Path = basepath / year / semester
+            class_paths: Path = get_class_paths(folder_path)
+            classes.update(dict(zip(class_list, class_paths)))
+
+    return classes
+
+
 def send_files(files_to_be_sent: list, send_enabled=False):
     for file in files_to_be_sent:
         send_file(file["src"], file["dst"], send_enabled=send_enabled)
 
 
 def get_class_paths(folder_path: Path):
-    classes = []
+    class_paths = []
     for folder in folder_path.iterdir():
         path: Path = folder_path / folder
         if path.is_dir():
-            classes.append(path)
+            class_paths.append(path)
 
-    return classes
+    return class_paths
 
 
 def traverse_folder(src_folder_path: Path, class_folders: dict) -> list[dict]:
     files_to_be_sent = []
 
     for file in src_folder_path.iterdir():
-        file_name_1 = file.name[:9].replace(" ", "").replace("-", "").replace("_", "").upper().strip()
+        tag = file.name[:9].replace(" ", "").replace("-", "").replace("_", "").upper().strip()
 
-        if file_name_1 in class_folders:
+        if tag in class_folders:
             files_to_be_sent.append({
                                     "src": file,
-                                    "dst": class_folders[file_name_1] / file.name,
+                                    "dst": class_folders[tag] / file.name,
                                     })
         elif file.is_dir():
             files_to_be_sent += traverse_folder(file, class_folders)

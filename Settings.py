67ec93d2183ad9ccf865613ed99a76f60f228b3c
Justin Morrison <morrisonxjustin@gmail.com>
2025-02-02 02:35:57 -0500

    Unimportant format changes

--- Settings.py
+++ Settings.py
@@ -1,9 +1,10 @@
 import json
 from pathlib import Path
 
+
 class Settings():
     JSON_FILE = "json_file"
-    
+
     WIN_BASEPATH = "win_basepath"
     WIN_SRC_PATH = "win_src_path"
     WIN_DST_PATH = "win_dst_path"
@@ -17,7 +18,7 @@ class Settings():
     def __init__(self, settings_file, current_file):
         with open(settings_file, 'r') as settings_json:
             settings: dict = json.load(settings_json)[current_file]
-        
+
         if Path.cwd().anchor == '/':
             self.basepath = Path(settings.get(Settings.WSL_BASEPATH, "NULL"))
             self.src_path = Path(settings.get(Settings.WSL_SRC_PATH, "NULL"))
@@ -35,10 +36,9 @@ class Settings():
         if not self.dst_path.exists() and self.dst_path.name != "NULL":
             raise FileNotFoundError(self.dst_path)
 
-
         self.json_file = Path(settings.get(Settings.JSON_FILE, "NULL"))
         if not self.json_file.exists() and self.json_file.name != "NULL":
             raise FileNotFoundError(self.json_file)
-        
+
         self.paths_to_check = settings.get(Settings.PATHS_TO_CHECK, "NULL")
         self.root = "NULL"

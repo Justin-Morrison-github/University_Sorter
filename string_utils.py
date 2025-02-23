3d1212e9ee9ad8931d599125d8b7bc1e5f311d00
Justin Morrison <morrisonxjustin@gmail.com>
2025-02-23 17:13:58 -0500

    Removed uncessary body from pretty_print_substring, now just calls pretty_substring. Added underline, color, and underline formatting functions.

--- string_utils.py
+++ string_utils.py
@@ -1,18 +1,14 @@
 from colorama import Fore
 
-
-def pretty_print_substring(str: str, substr: str, start="\u2794  ", end="", color=Fore.YELLOW):
-    str_len = len(str)
-    substr_len = len(substr)
-
-    index = str.lower().find(substr.lower())
-
-    string = start + str[0: index] + color + str[index: index + substr_len] + Fore.RESET + str[index +
-                                                                                               substr_len: str_len] + end
-    print(string)
+UNDERLINE = "\033[4m"
+RESET = "\033[0m"
 
 
-def pretty_print_string(str: str, substr: str, start: str, end="", color=Fore.YELLOW):
+def pretty_print_substring(str: str, substr: str, start="", end="", color=Fore.YELLOW):
+    print(pretty_substring(str, substr, start, end, color))
+
+
+def pretty_substring(str: str, substr: str, start: str = "", end="", color=Fore.YELLOW):
     str_len = len(str)
     substr_len = len(substr)
 
@@ -21,3 +17,17 @@ def pretty_print_string(str: str, substr: str, start: str, end="", color=Fore.YE
     string = start + str[0: index] + color + str[index: index + substr_len] + Fore.RESET + str[index +
                                                                                                substr_len: str_len] + end
     return string
+
+
+def underline(string: str):
+    return f"{UNDERLINE}{string}{RESET}"
+
+def color(string: str, color):
+    return f"{color}{string}{Fore.RESET}"
+
+def underline_color(string:str, color = Fore.YELLOW):
+    return f"{UNDERLINE}{color}{string}{RESET}"
+
+
+def prepend(string:str, start:str):
+    return start + string

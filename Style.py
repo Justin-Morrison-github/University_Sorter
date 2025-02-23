9a20e26fecf078c7bd57486e2f57a72f505d53cb
Justin Morrison <morrisonxjustin@gmail.com>
2025-02-23 17:11:48 -0500

    File to hold commonly used symbols/colors for printing in the terminal.

---
+++ Style.py
@@ -0,0 +1,29 @@
+from enum import StrEnum
+from colorama import Fore
+
+
+class Style(StrEnum):
+    # ANSI escape Sequences
+    UNDERLINE = "\033[4m"
+    RESET = "\033[0m"
+
+    # Special Characters/Symbols
+    ARROW = "⮡ "
+    STRAIGHT_ARROW = "➔ "
+    SUCCESS_CHAR = "☑ "
+    FAILURE_CHAR = "☒ "
+    WARNING_CHAR = "⚠ "
+    INDENT = "    "
+    B_FAILURE = "❌ "
+    B_SUCCESS = "✅ "
+
+    # Colors with symbols
+    SUCCESS = f"{Fore.GREEN}{SUCCESS_CHAR}"
+    WARNING = f"{Fore.YELLOW}{WARNING_CHAR}"
+    FAILURE = f"{Fore.RED}{FAILURE_CHAR}"
+
+    # Symbols with colors and indents
+    TAB_ARROW = f"{INDENT}{ARROW}"
+    TAB_SUCCESS = f'{INDENT}{SUCCESS}'
+    TAB_FAILURE = f'{INDENT}{FAILURE}'
+    TAB_WARNING = f'{INDENT}{WARNING}'

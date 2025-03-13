ae00dec83a02425a9380c12ea6c31d99b016af66
Justin Morrison <morrisonxjustin@gmail.com>
2025-03-12 23:53:47 -0400

    Added short names for arrows with colors

--- app/src/Style.py
+++ app/src/Style.py
@@ -22,8 +22,14 @@ class Style(StrEnum):
     WARNING = f"{Fore.YELLOW}{WARNING_CHAR}"
     FAILURE = f"{Fore.RED}{FAILURE_CHAR}"
 
+    SUCCESS_ARROW = f"{Fore.GREEN}{ARROW}"
+    WARNING_ARROW = f"{Fore.YELLOW}{ARROW}"
+    FAILURE_ARROW = f"{Fore.RED}{ARROW}"
+
     # Symbols with colors and indents
     TAB_ARROW = f"{INDENT}{ARROW}"
     TAB_SUCCESS = f'{INDENT}{SUCCESS}'
     TAB_FAILURE = f'{INDENT}{FAILURE}'
     TAB_WARNING = f'{INDENT}{WARNING}'
+
+    TAB_YELLOW_ARROW = f'{INDENT}{WARNING_ARROW}'
